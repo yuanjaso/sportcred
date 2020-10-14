b279a65c7e3b5061387e08299d916a1ff23f036b
Jason Yuan <yuanjason98@gmail.com>
2020-10-14 17:37:48 -0400

    [LIL-37] added store dev tools

--- frontend/lilypad-sportcred-frontend/src/app/app.module.ts
+++ frontend/lilypad-sportcred-frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,6 +23,10 @@ import { ZoneModule } from './zone/zone.module';
     PicksModule,
     TriviaModule,
     ZoneModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
