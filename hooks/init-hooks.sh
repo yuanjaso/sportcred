2b8392bc83d2b90f48164f240936302724a8a6e5
Jason Yuan <yuanjason98@gmail.com>
2020-10-14 13:15:03 -0400

    [LIL-47] set up git hooks

---
+++ hooks/init-hooks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# hooks directory
+HOOKS_DIRECTORY=$(pwd)
+
+# point git to look at this directory for the hooks
+git config core.hooksPath $HOOKS_DIRECTORY
+
+# need to give these files executable permission to run their sccripts
+chmod +x prepare-commit-msg 
+chmod +x pre-commit prepare-commit-msg
