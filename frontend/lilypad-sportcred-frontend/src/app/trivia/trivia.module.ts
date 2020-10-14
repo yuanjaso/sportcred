c0276f80417e5992ca68dc2c12684ec5b7150080
Jason Yuan <yuanjason98@gmail.com>
2020-10-14 16:31:44 -0400

    [LIL-37] preformatting commit

--- frontend/lilypad-sportcred-frontend/src/app/trivia/trivia.module.ts
+++ frontend/lilypad-sportcred-frontend/src/app/trivia/trivia.module.ts
@@ -1,14 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { TriviaRoutingModule } from './trivia-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    TriviaRoutingModule
-  ]
+  imports: [CommonModule, TriviaRoutingModule],
 })
-export class TriviaModule { }
+export class TriviaModule {}
