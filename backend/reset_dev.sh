cb5a11bb97bce5cac6cb66ba6ec4061bf9fb5201
Michael Do <do.michael.7550@gmail.com>
2020-10-18 19:12:21 -0400

    [LIL-52] Implement test suite for sports, teams, players end point

--- backend/reset_dev.sh
+++ backend/reset_dev.sh
@@ -8,6 +8,8 @@
     mkdir -p tmp
 
     python3 manage.py migrate --run-syncdb
+    python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('poop', 'admin@example.com', '.')"
+    python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.create_user('bbb', 'test@example.com', '.'); from rest_framework.authtoken.models import Token; Token.objects.create(user=u)"    
     python3 manage.py import_questionnaire database_files/questionnaire.yaml 
     python3 manage.py import_sports database_files/sports.yaml
 
