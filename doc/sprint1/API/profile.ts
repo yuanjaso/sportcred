2d1d04afcdfa839677ae21200147b100d5d2f769
OhmNegi <39748691+OhmNegi@users.noreply.github.com>
2020-10-19 20:55:58 -0400

    updated profile api

--- doc/sprint1/API/profile.ts
+++ doc/sprint1/API/profile.ts
@@ -1,13 +1,13 @@
-import { API } from './restapi';
+import { API } from "./restapi";
 
 const apiRequirements: API[] = [
   {
-    description: 'Update status',
+    description: "Update status",
     request: {
-      requestURL: '/api/v1/profile/status/',
-      requestMethod: 'PUT',
+      requestURL: "/api/v1/profile/status/",
+      requestMethod: "PUT",
       body: {
-        status: 'Hi guys, I like sportcred!',
+        status: "Hi guys, I like sportcred!",
       },
       queryParams: {},
     },
@@ -17,29 +17,33 @@ const apiRequirements: API[] = [
     },
   },
   {
-    description: 'Upload Profile Picture',
+    description: "Upload Profile Picture",
     request: {
-      requestURL: '/api/v1/profile/picture/',
-      requestMethod: 'PUT',
+      requestURL: "/api/v1/profile/picture/",
+      requestMethod: "PUT",
       body: {
-        picture: 'placeholder',
+        picture: "placeholder",
       },
       queryParams: {},
     },
     response: {
       statusCode: 200,
       response: {
-        pictureURL: 'somethingsomething.png',
+        name: "somethingsomething.png",
+        content_type: "image/png",
+        charset: "None",
+        url:
+          "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
       },
     },
   },
   {
-    description: 'Update About Section',
+    description: "Update About Section",
     request: {
-      requestURL: '/api/v1/profile/about/',
-      requestMethod: 'PUT',
+      requestURL: "/api/v1/profile/about/",
+      requestMethod: "PUT",
       body: {
-        about: 'placeholder',
+        about: "placeholder",
       },
       queryParams: {},
     },
@@ -49,10 +53,10 @@ const apiRequirements: API[] = [
     },
   },
   {
-    description: 'Get the profile of a user',
+    description: "Get the profile of a user",
     request: {
-      requestURL: '/api/v1/profile/',
-      requestMethod: 'GET',
+      requestURL: "/api/v1/profile/",
+      requestMethod: "GET",
       body: null,
       queryParams: {
         userId: 4,
@@ -61,12 +65,19 @@ const apiRequirements: API[] = [
     response: {
       statusCode: 200,
       response: {
-        status: 'Sportcred is the best !!!!!',
-        acs: 599,
-        acsHistory: [900, -1, 34, 23, 43, -23],
-        profilePicture: 'placeholderpicture.png',
-        // TODO figure out what is part of about
-        about: null,
+        user: { id: 4, username: "michael", email: "michael_doughs@gmail.com" },
+        status: "Sportcred is the best !!!!!",
+        profilepicture: {
+          name: "somethingsomething.png",
+          content_type: "image/png",
+          charset: "None",
+          url:
+            "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
+        },
+        //ACS is on hold for this sprint
+        //acs: 599,
+        //acsHistory: [900, -1, 34, 23, 43, -23],
+        about: "placeholder",
       },
     },
   },
