285a9c01e9756b524c7a62a2008884a5039c6daa
Jason Yuan <yuanjason98@gmail.com>
2020-10-21 18:21:20 -0400

    [LIL-30] Update profile.ts

--- doc/sprint1/API/profile.ts
+++ doc/sprint1/API/profile.ts
@@ -2,6 +2,34 @@ import { API } from "./restapi";
 
 const apiRequirements: API[] = [
   {
+    description: "Get the profile of a user",
+    request: {
+      requestURL: "/api/v1/profile/",
+      requestMethod: "GET",
+      body: null,
+      queryParams: {
+        user_id: 4,
+      },
+    },
+    response: {
+      statusCode: 200,
+      response: {
+        user: { id: 4, username: "michael", is_superuser: true },
+        status: "Sportcred is the best !!!!!",
+        profilepicture:
+          "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
+        about: "placeholder",
+        highlights: [
+          { id: 1, name: "Basketball" },
+          { id: 2, name: "Baseball" },
+        ],
+        //ACS is on hold for this sprint
+        // acs: 599,
+        //acsHistory: [900, -1, 34, 23, 43, -23],
+      },
+    },
+  },
+  {
     description: "Update Status/Picture/About/ or Highlights",
     request: {
       requestURL: "/api/v1/profile/", // dont need id since you're only patching yourself
@@ -10,7 +38,7 @@ const apiRequirements: API[] = [
         profilepicture: "newpic.jpg",
         status: "Hi guys, I like sportcred!", // only one of these fields need to be here. patch is partial update
         about: "placeholder",
-        highlights: [1, 2], // array of sports_id's
+        favourite_sports: [1, 2], // array of sports_id's
         // friends list is kinda nasty since it can get really big. it needs to be paginated so it will get its own endpoint
       },
       queryParams: {},
@@ -19,23 +47,18 @@ const apiRequirements: API[] = [
       // all response body's should be the same. we can use the same serializer to normalize it
       statusCode: 200,
       response: {
-        user: { id: 4, username: "michael", email: "michael_doughs@gmail.com" },
+        user: { id: 4, username: "michael", is_superuser: false },
         status: "Hi guys, I like sportcred!",
-        profilepicture: {
-          name: "somethingsomething.png",
-          content_type: "image/png",
-          charset: "None",
-          url:
-            "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
-        },
-        //ACS is on hold for this sprint
-        //acs: 599,
-        //acsHistory: [900, -1, 34, 23, 43, -23],
+        profilepicture:
+          "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
         about: "placeholder",
-        highlights: [
+        favourite_sports: [
           { id: 1, name: "Basketball" },
           { id: 2, name: "Baseball" },
         ],
+        //ACS is on hold for this sprint
+        //acs: 599,
+        //acsHistory: [900, -1, 34, 23, 43, -23],
       },
     },
   },
@@ -60,34 +83,7 @@ const apiRequirements: API[] = [
       },
     },
   },
-  {
-    description: "Get the profile of a user",
-    request: {
-      requestURL: "/api/v1/profile/",
-      requestMethod: "GET",
-      body: null,
-      queryParams: {
-        user_id: 4,
-      },
-    },
-    response: {
-      statusCode: 200,
-      response: {
-        user: { id: 4, username: "michael", is_superuser: true },
-        status: "Sportcred is the best !!!!!",
-        profilepicture:
-          "/media/user/pictures/f3/0a/f30a2b6329a1bb5d867848c53d3581b5d1068d85475fa0699283a63e9f5a6c7c.png",
-        //ACS is on hold for this sprint
-        // acs: 599,
-        //acsHistory: [900, -1, 34, 23, 43, -23],
-        about: "placeholder",
-        highlights: [
-          { id: 1, name: "Basketball" },
-          { id: 2, name: "Baseball" },
-        ],
-      },
-    },
-  },
+
   {
     description: "See followers, and following",
     request: {
