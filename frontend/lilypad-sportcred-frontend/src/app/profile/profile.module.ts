d8ac5137a2d00731a8d7550c7d43f5ea6e6ea405
Jason Yuan <yuanjason98@gmail.com>
2020-10-21 20:18:13 -0400

    [LIL-8] moved imports

--- frontend/lilypad-sportcred-frontend/src/app/profile/profile.module.ts
+++ frontend/lilypad-sportcred-frontend/src/app/profile/profile.module.ts
@@ -1,14 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { MaterialModule } from '../../global/material/material.module';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ProfileService } from './profile.service';
@@ -18,12 +13,7 @@ import { profileFeatureKey, profileReducer } from './store/profile.reducer';
 @NgModule({
   declarations: [ProfileComponent],
   imports: [
-    MatChipsModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatFormFieldModule,
+    MaterialModule,
     FormsModule,
     StoreModule.forFeature(profileFeatureKey, profileReducer),
     EffectsModule.forFeature([ProfileEffects]),
