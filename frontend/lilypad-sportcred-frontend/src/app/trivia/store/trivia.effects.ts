9fc659fe432bf47a69122b6a2445da7bba092c71
Jason Yuan <yuanjason98@gmail.com>
2020-10-27 10:28:20 -0400

    [LIL-75] renamed files

--- frontend/lilypad-sportcred-frontend/src/app/trivia/store/trivia.effects.ts
+++ frontend/lilypad-sportcred-frontend/src/app/trivia/store/trivia.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import * as TriviaActions from './actions';
+import * as TriviaActions from './trivia.actions';
 
 @Injectable()
 export class TriviaEffects {
