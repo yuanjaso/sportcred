63d24bf79cc8a8bfc68c916ed511f1cf73dac96e
Jason Yuan <yuanjason98@gmail.com>
2020-10-29 11:30:49 -0400

    [LIL-84] added badge (notif) to bell icon

--- frontend/lilypad-sportcred-frontend/src/app/header/header.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/header/header.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
+  notificationsCount: number;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // ! hacking cuz development
+    this.notificationsCount = 7;
+  }
 
   showNotificationsModal(): void {
     console.log('TODO: show notifications modal');
