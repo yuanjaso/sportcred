a42bec2e249d4fcc82e7f2299481a8d7cfb2695c
Jason Yuan <yuanjason98@gmail.com>
2020-10-29 10:52:11 -0400

    [LIL-84] removed old code

--- frontend/lilypad-sportcred-frontend/src/app/trivia/store/trivia.selectors.ts
+++ frontend/lilypad-sportcred-frontend/src/app/trivia/store/trivia.selectors.ts
@@ -1,11 +1,6 @@
 import { createSelector } from '@ngrx/store';
 import { selectFeatureTrivia } from '../../store/selectors';
 
-export const selectTriviaQuestions = createSelector(
-  selectFeatureTrivia,
-  (state) => state.triviaQuestions
-);
-
 export const selectUpdatedACS = createSelector(
   selectFeatureTrivia,
   (state) => state.updatedACS
