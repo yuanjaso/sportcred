2400b6e667dafc01e192a85c0afe6fad16340a77
Maggie Dang <maggiedang11@gmail.com>
2020-11-01 02:52:44 -0500

    [LIL-74] Add countdown timer view

--- frontend/lilypad-sportcred-frontend/src/app/trivia/single-trivia/single-trivia.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/trivia/single-trivia/single-trivia.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-single-trivia',
   templateUrl: './single-trivia.component.html',
-  styleUrls: ['./single-trivia.component.scss']
+  styleUrls: ['./single-trivia.component.scss'],
 })
 export class SingleTriviaComponent implements OnInit {
+  singleQuestionTime = 14;
+  numberOfQuestions = 10;
+  displayTimer = '00:00';
 
-  constructor() { }
+  triviaAnswers: { answer: string }[] = [
+    { answer: "Answer 1"},
+    { answer: "Answer 2"},
+    { answer: "Answer 3"},
+    { answer: "Answer 4"},
+  ]
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.questionTimer(this.singleQuestionTime);
   }
 
+  questionTimer(timePerQuestion: number): void {
+    const seconds$ = interval(1000);
+    //const totalTime$ = timer(timePerQuestion * 1000 * this.numberOfQuestions);
+
+    const timer$ = seconds$.pipe(take(timePerQuestion + 1));
+
+    const subscriber = timer$.subscribe((val) => {
+      //console.log(timePerQuestion - val);
+      const result = timePerQuestion - val;
+      this.displayTimer = new Date(result * 1000).toISOString().substr(14, 5);
+    });
+  }
 }
