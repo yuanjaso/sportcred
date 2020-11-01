83a44cfb1f74ad911054c0e89d599b27b879d36f
Maggie Dang <maggiedang11@gmail.com>
2020-10-31 21:33:48 -0400

    [LIL-74] Trivia question and answers basic layout with static examples

--- frontend/lilypad-sportcred-frontend/src/global/material/material.module.ts
+++ frontend/lilypad-sportcred-frontend/src/global/material/material.module.ts
@@ -7,14 +7,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 const modules = [
   MatCardModule,
   MatFormFieldModule,
@@ -32,6 +33,7 @@ const modules = [
   MatIconModule,
   MatSelectModule,
   MatListModule,
+  MatGridListModule,
 ];
 @NgModule({
   declarations: [],
