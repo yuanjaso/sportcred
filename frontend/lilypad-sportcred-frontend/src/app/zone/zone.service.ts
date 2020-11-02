e621c9b6feb2d02f18520acce162930707395729
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-02 18:01:59 -0500

    remove unused var

--- frontend/lilypad-sportcred-frontend/src/app/zone/zone.service.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/zone.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 import { playersURL, sportsURL, teamsURL } from '../../global/api.types';
 import { HttpClientWrapper } from '../http/http-client-wrapper';
 @Injectable({
@@ -7,13 +6,6 @@ import { HttpClientWrapper } from '../http/http-client-wrapper';
 })
 export class ZoneService {
   constructor(private http: HttpClientWrapper) {}
-
-  /*SUBJECTS */
-
-  //emit false to contract,
-  //emit true to expand,
-  //emit nothing to toggle
-  $sidenavToggle = new Subject<boolean>();
 
   /* HTTP REQUESTS*/
   getAllPlayers() {
