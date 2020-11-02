f4b3bf0ff5d8799bdc597b1fc832d684010c43e4
Maggie Dang <maggiedang11@gmail.com>
2020-11-02 01:37:17 -0500

    [LIL-74] Get and display triviaInstance data on views and check for answer clicked correctness

--- frontend/lilypad-sportcred-frontend/src/app/trivia/single-trivia/single-trivia.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/trivia/single-trivia/single-trivia.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Subscription, timer } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { interval, Observable, Subscription, timer } from 'rxjs';
 import { take } from 'rxjs/operators';
+import { AppState } from 'src/app/store/reducer';
+import { setTriviaInstance } from '../store/trivia.actions';
+import { selectTriviaInstance } from '../store/trivia.selectors';
+import { TriviaInstance, TriviaQuestion } from '../trivia.types';
 
 @Component({
   selector: 'app-single-trivia',
@@ -10,24 +15,107 @@ import { take } from 'rxjs/operators';
 export class SingleTriviaComponent implements OnInit {
   singleQuestionTime = 14;
   numberOfQuestions = 10;
-  currentQuestion = 1;
+  currentQuestion = 0;
   displayTimer = '00:00';
+  displayElementId = 'main-display';
 
   timerTickSubscriber: Subscription;
   timerQuestionSubscriber: Subscription;
 
-  triviaAnswers: { answer: string }[] = [
-    { answer: 'Answer 1' },
-    { answer: 'Answer 2' },
-    { answer: 'Answer 3' },
-    { answer: 'Answer 4' },
+  triviaInstance$: Observable<TriviaInstance>;
+  questions: TriviaQuestion[];
+
+  triviaAnswers: { id: number; answer_content: string }[] = [
+    { id: 0, answer_content: '' },
+    { id: 0, answer_content: '' },
+    { id: 0, answer_content: '' },
+    { id: 0, answer_content: '' },
   ];
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.timerTickSubscriber = this.tickTimer();
     this.timerQuestionSubscriber = this.questionTimer();
+
+    this.pullingTriviaData();
+    this.triviaInstance$.subscribe((val) => {
+      this.questions = val.questions;
+      this.displayQuestion(0);
+    });
+  }
+
+  // TODO: replace hardcode with backend
+  pullingTriviaData(): void {
+    this.triviaInstance$ = this.store.select(selectTriviaInstance);
+
+    this.store.dispatch(
+      setTriviaInstance({
+        triviaInstance: {
+          date: new Date().toISOString(),
+          id: 3,
+          is_completed: false,
+          sport: { id: 3, name: 'Basketball' },
+          questions: [
+            {
+              id: 4,
+              answers: [
+                { id: 4, answer_content: 'Kobe' },
+                { id: 5, answer_content: 'Jordan' },
+                { id: 6, answer_content: 'LeBron' },
+                { id: 7, answer_content: 'Harden' },
+              ],
+              correct_answer: { id: 4, answer_content: 'Kobe' },
+              question_content: 'Who is the GOAT?',
+            },
+            {
+              id: 4,
+              answers: [
+                { id: 4, answer_content: 'LeBron' },
+                { id: 5, answer_content: 'Jordan' },
+                { id: 6, answer_content: 'LeBron' },
+                { id: 7, answer_content: 'LeBron' },
+              ],
+              correct_answer: { id: 4, answer_content: 'Kobe' },
+              question_content: 'Who is the GOAT? 2',
+            },
+            {
+              id: 4,
+              answers: [
+                { id: 4, answer_content: 'Kobe' },
+                { id: 5, answer_content: 'Jordan' },
+                { id: 6, answer_content: 'LeBron' },
+                { id: 7, answer_content: 'LeBron' },
+              ],
+              correct_answer: { id: 4, answer_content: 'Kobe' },
+              question_content: 'Who is the GOAT? 3',
+            },
+            {
+              id: 4,
+              answers: [
+                { id: 4, answer_content: 'Kobe' },
+                { id: 5, answer_content: 'Kobe' },
+                { id: 6, answer_content: 'Kobe' },
+                { id: 7, answer_content: 'Harden' },
+              ],
+              correct_answer: { id: 4, answer_content: 'Kobe' },
+              question_content: 'Who is the GOAT? 4',
+            },
+          ],
+          user: { id: 4, username: 'LeBron' },
+          other_user: { id: 45, username: 'Jordan' },
+        },
+      })
+    );
+  }
+
+  displayQuestion(index: number) {
+    // Question
+    document.getElementById(this.displayElementId).innerHTML = this.questions[
+      index
+    ].question_content;
+    // Answers
+    this.triviaAnswers = this.questions[index].answers;
   }
 
   /**
@@ -56,29 +144,34 @@ export class SingleTriviaComponent implements OnInit {
     const subscriber = questionTimer$.subscribe((val) => {
       // The user has ran out of time and has not clicked on an answer
       console.log('question timer up');
+      // Update score
       // Next question
       this.currentQuestion += 1;
-      // restart timer
-      if (this.currentQuestion <= this.numberOfQuestions) {
+      if (this.currentQuestion < this.numberOfQuestions) {
+        this.displayQuestion(this.currentQuestion);
         this.restartTimers();
       }
     });
     return subscriber;
   }
 
-  onClickAnswer() {
+  onClickAnswer(answerId: number) {
     if (
       !this.timerTickSubscriber.closed &&
       !this.timerQuestionSubscriber.closed &&
-      this.currentQuestion <= this.numberOfQuestions
+      this.currentQuestion < this.numberOfQuestions
     ) {
       // Stop timers
       this.timerTickSubscriber.unsubscribe();
       this.timerQuestionSubscriber.unsubscribe();
       // Check if correct answer
+      if (this.isCorrect(this.currentQuestion, answerId)) {
+        console.log('Correct!');
+      }
       // Add to final score
       // Go to next question
       this.currentQuestion += 1;
+      this.displayQuestion(this.currentQuestion);
       // Restart timers
       this.restartTimers();
     }
@@ -87,6 +180,11 @@ export class SingleTriviaComponent implements OnInit {
       // Show final results
       console.log('show Results');
     }
+  }
+
+  isCorrect(questionIndex: number, answerId: number): boolean {
+    const actualAnswerId = this.questions[questionIndex].correct_answer.id;
+    return actualAnswerId == answerId;
   }
 
   restartTimers() {
