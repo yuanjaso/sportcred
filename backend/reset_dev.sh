2aefafa8a05ff43c85e8420381c7fe48f75317f4
Michael Do <do.michael.7550@gmail.com>
2020-11-01 22:13:22 -0500

    [LIL-2] fix reset_dev.sh to actually work by removing the profile setup

--- backend/reset_dev.sh
+++ backend/reset_dev.sh
@@ -8,7 +8,7 @@
     mkdir -p tmp
 
     python3 manage.py migrate --run-syncdb
-    python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.create_superuser('poop', 'admin@example.com', '.');from rest_framework.authtoken.models import Token; Token.objects.create(user=u); from sportscred.models import Profile; Profile.objects.create(user=u);"
+    python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.create_superuser('poop', 'admin@example.com', '.');from rest_framework.authtoken.models import Token; Token.objects.create(user=u);"
     python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.create_user('bbb', 'test@example.com', '.'); from rest_framework.authtoken.models import Token; Token.objects.create(user=u); from sportscred.models import Profile; Profile.objects.create(user=u);"   
     echo "Database resynched"
     
@@ -17,7 +17,11 @@
     
     echo "importing sports"
     python3 manage.py import_sports database_files/sports.yaml
+
+    echo "importing profile"
     python3 manage.py import_profile database_files/profile.yaml
+
+    echo "importing acshistory"
     python3 manage.py import_baseacshistory database_files/baseacshistory.yaml
 
     echo "importing trivia"
