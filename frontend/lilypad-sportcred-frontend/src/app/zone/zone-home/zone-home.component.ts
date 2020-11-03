1614ca9f948905fdede6d99a62ffc0ab644d1640
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-02 19:34:19 -0500

    implement base graph, made profiles responsive

--- frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/zone-home.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/zone-home.component.ts
@@ -22,23 +22,7 @@ export class ZoneHomeComponent implements OnInit, OnDestroy {
   constructor(
     private zoneService: ZoneService,
     private breakpointObserver: BreakpointObserver
-  ) {
-    this.chartOption = {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line',
-        },
-      ],
-    };
-  }
+  ) {}
 
   ngOnInit(): void {
     this.subcriptions.add(
--- frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/zone-home.module.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/zone-home.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '../../../global/material/material.module';
-import { SharedModule } from '../../shared-components/shared-components.module';
 import { PicksModule } from './subpages/picks/picks.module';
 import { TriviaModule } from './subpages/trivia/trivia.module';
 import { ZoneHomeRoutingModule } from './zone-home-routing.module';
@@ -14,7 +13,6 @@ import { ZoneHomeComponent } from './zone-home.component';
     ZoneHomeRoutingModule,
     PicksModule,
     TriviaModule,
-    SharedModule,
   ],
 })
 export class ZoneHomeModule {}
