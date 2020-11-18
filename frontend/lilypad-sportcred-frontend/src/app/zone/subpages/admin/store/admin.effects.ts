a564078ef214ac385f1fef672885a7d22454dc8d
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-17 19:34:14 -0500

    add a get debate afteraddition so view is synced

--- frontend/lilypad-sportcred-frontend/src/app/zone/subpages/admin/store/admin.effects.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/subpages/admin/store/admin.effects.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap, switchMap } from 'rxjs/operators';
+import { map, mergeMap, switchMap, catchError } from 'rxjs/operators';
 import { AdminService } from '../admin.service';
 import { getQuestionnaireResponse, submitDebatePost } from './admin.actions';
+import { EMPTY } from 'rxjs';
+import { getDebateTopics } from 'src/app/zone/zone-home/subpages/debate/store/debate.actions';
 
 @Injectable()
 export class AdminEffects {
@@ -21,11 +23,19 @@ export class AdminEffects {
   submitDebatePost$ = createEffect(() =>
     this.actions$.pipe(
       ofType(submitDebatePost),
-      mergeMap(({ submission }) =>
-        this.adminService.submitDebatePost(submission)
-      )
-    ),
-    { dispatch: false }
+      mergeMap(({ submission }) => {
+        return this.adminService.submitDebatePost(submission).pipe(
+          map((payload) => {
+            return {
+              type: getDebateTopics.type,
+            };
+          }),
+          catchError(() => {
+            return EMPTY;
+          })
+        );
+      })
+    )
   );
 
   constructor(private actions$: Actions, private adminService: AdminService) {}
