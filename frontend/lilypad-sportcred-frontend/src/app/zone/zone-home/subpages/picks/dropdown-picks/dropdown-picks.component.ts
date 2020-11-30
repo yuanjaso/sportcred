297ec03394e338c9a9964c3472800c9aa9907411
Jason Yuan <yuanjason98@gmail.com>
2020-11-30 10:54:09 -0500

    [LIL-123] retrieve player/rookie poll for mvp/rookie of the year picks

--- frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/subpages/picks/dropdown-picks/dropdown-picks.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/subpages/picks/dropdown-picks/dropdown-picks.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../../../store/reducer';
+import { getAllPlayers } from '../../../../store/actions';
+import { selectPlayers } from '../../../../store/selectors';
+import { Player } from '../../../../zone.types';
 
 @Component({
   selector: 'app-dropdown-picks',
   templateUrl: './dropdown-picks.component.html',
-  styleUrls: ['./dropdown-picks.component.scss']
+  styleUrls: ['./dropdown-picks.component.scss'],
 })
 export class DropdownPicksComponent implements OnInit {
+  players$: Observable<Player[]>;
+  rookies$: Observable<Player[]>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.grabDataForDropdowns();
   }
 
+  private grabDataForDropdowns(): void {
+    // the players list is already retrieved when the app loads so we just need to grab rookies
+    this.store.dispatch(getAllPlayers({ rookies: true }));
+    this.players$ = this.store.select(selectPlayers);
+    this.rookies$ = this.store.select(selectPlayers);
+  }
 }
