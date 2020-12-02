26544e0d32b598f380e720590626f3f6b5ec9558
Jason Yuan <yuanjason98@gmail.com>
2020-12-02 11:28:23 -0500

    [LIL-123] general changes for playoff bracket
    - improved first round matchup typing/data
    - removed race cond for accessing preds while undefined
    - moved example submit call

--- frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/subpages/picks/tree-picks/tree-picks.component.ts
+++ frontend/lilypad-sportcred-frontend/src/app/zone/zone-home/subpages/picks/tree-picks/tree-picks.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { groupBy } from 'lodash';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { cloneDeep } from 'lodash';
+import { Subscription } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 import { AppState } from '../../../../../store/reducer';
-import { selectTeams } from '../../../../store/selectors';
-import { Team } from '../../../../zone.types';
 import {
   PlayoffPrediction,
   PlayoffRound,
@@ -21,7 +19,8 @@ import { selectPredictions } from '../store/picks.selectors';
   templateUrl: './tree-picks.component.html',
   styleUrls: ['./tree-picks.component.scss'],
 })
-export class TreePicksComponent implements OnInit, PredictionFeature {
+export class TreePicksComponent
+  implements OnInit, OnDestroy, PredictionFeature {
   // two cases
   // if admin page, don't display the current result
   // if not admin page, display the current result if exists
@@ -30,45 +29,64 @@ export class TreePicksComponent implements OnInit, PredictionFeature {
 
   year: number = new Date().getFullYear();
   sport: 'Basketball' = 'Basketball';
+  // TODO: fill this out with what the user selected
   playoff: { id: number; team: number }[];
 
-  teams$: Observable<Team[]>;
   // this will give all you need to get the user's picks from the past
   existingPlayoffPredictions: {
     [round in PlayoffRound]?: PlayoffPrediction;
   } = {};
   // use this to determine who is on the first round , it is mocked but fine for the minimum viable product
-  firstRound: { [round in PlayoffRound]?: { home: string; away: string } } = {
-    'east_first_round-1-8': { home: 'Milwaukee Bucks', away: 'Orlando Magic' },
-    'east_first_round-2-7': { home: 'Toronto Raptors', away: 'Brooklyn Nets' },
-    'east_first_round-3-6': {
-      home: 'Boston Celtics',
-      away: 'Philadelphia 76ers',
+  firstRound: {
+    [round in PlayoffRound]?: {
+      home: { id: number; name: string };
+      away: { id: number; name: string };
+    };
+  } = {
+    'east_first_round-1-8': {
+      home: { name: 'Milwaukee Bucks', id: 1 },
+      away: { name: 'Orlando Magic', id: 2 },
     },
-    'east_first_round-4-5': { home: 'Indiana Pacers', away: 'Miami Heat' },
+    'east_first_round-2-7': {
+      home: { name: 'Toronto Raptors', id: 3 },
+      away: { name: 'Brooklyn Nets', id: 4 },
+    },
+    'east_first_round-3-6': {
+      home: { name: 'Boston Celtics', id: 5 },
+      away: { name: 'Philadelphia 76ers', id: 6 },
+    },
+    'east_first_round-4-5': {
+      home: { name: 'Indiana Pacers', id: 7 },
+      away: { name: 'Miami Heat', id: 8 },
+    },
     'west_first_round-1-8': {
-      home: 'Los Angeles Lakers',
-      away: 'Portland Trailblazers',
+      home: { name: 'Los Angeles Lakers', id: 9 },
+      away: { name: 'Portland Trailblazers', id: 10 },
     },
     'west_first_round-2-7': {
-      home: 'Los Angeles Clippers',
-      away: 'Dallas Mavericks',
+      home: { name: 'Los Angeles Clippers', id: 11 },
+      away: { name: 'Dallas Mavericks', id: 12 },
     },
-    'west_first_round-3-6': { home: 'Denver Nuggets', away: 'Utah Jazz' },
+    'west_first_round-3-6': {
+      home: { name: 'Denver Nuggets', id: 13 },
+      away: { name: 'Utah Jazz', id: 14 },
+    },
     'west_first_round-4-5': {
-      home: 'Houston Rockets',
-      away: 'Oklahoma City Thunder',
+      home: { name: 'Houston Rockets', id: 15 },
+      away: { name: 'Oklahoma City Thunder', id: 16 },
     },
   };
+
+  private subscription = new Subscription();
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.setPreviousPrediction();
-    this.teams$ = this.store.select(selectTeams);
+  }
 
-    // ! HARDCODED MOCK DATA FOR DEMO
-    this.exampleSubmit();
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   submit(
@@ -97,23 +115,21 @@ export class TreePicksComponent implements OnInit, PredictionFeature {
   }
 
   private setPreviousPrediction(): void {
-    this.store
-      .select(selectPredictions)
-      .pipe(
-        tap(({ year, sport, playoff }) => {
-          this.year = year;
-          this.sport = sport;
-          this.existingPlayoffPredictions = {};
-          playoff.forEach((round) => {
-            this.existingPlayoffPredictions[round.title] = round;
-          });
-        })
-      )
-      .subscribe();
-  }
-
-  private exampleSubmit(): void {
-    this.playoff = [];
-    this.submit(this.year, this.sport, this.playoff);
+    const sub$ = this.store.select(selectPredictions).pipe(
+      filter((data) => data !== undefined),
+      map(cloneDeep),
+      tap(({ year, sport, playoff }) => {
+        this.year = year;
+        this.sport = sport;
+        // initial setting of playoff will be the user's predictions,
+        // if they don't modify and resubmit then we will send back the unchanged predictions
+        this.playoff = playoff;
+        this.existingPlayoffPredictions = {};
+        playoff.forEach((round) => {
+          this.existingPlayoffPredictions[round.title] = round;
+        });
+      })
+    );
+    this.subscription.add(sub$.subscribe());
   }
 }
